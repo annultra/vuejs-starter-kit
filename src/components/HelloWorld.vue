f666726ca4e357320ca80f8706c48024991e44f7
Stefan Frede <stefan@frede.dev>
2020-10-19 22:28:59 +0200

    feat: implement tailwindcss (#2)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>
-    {{ msg }}
-  </h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div class="text-center mt-6">
+    <h1
+      class="text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+    >
+      {{ msg }}
+    </h1>
+    <button
+      type="button"
+      class="mt-4 inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+      @click="count++"
+    >
+      Count is: {{ count }}
+    </button>
+    <p class="mt-4 text-lg leading-7 text-gray-500">
+      Edit
+      <code class="italic text-gray-600">components/HelloWorld.vue</code> to
+      test hot module replacement.
+    </p>
+  </div>
 </template>
 
 <script>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  future: 'all',
+  purge: ['./src/**/*.js', './src/**/*.vue'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
