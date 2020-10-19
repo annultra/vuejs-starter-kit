a5a44f9cfdd05fa745cd6214c8f9346cebd4eab3
Stefan Frede <stefan@frede.dev>
2020-10-19 21:55:18 +0200

    feat: implement code linting (#1)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,7 @@
   <h1>
     {{ msg }}
   </h1>
-  <button @click="count++">
-    count is: {{ count }}
-  </button>
+  <button @click="count++">count is: {{ count }}</button>
   <p>
     Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
@@ -14,12 +12,15 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      default: '',
+    },
   },
   data() {
     return {
-      count: 0
-    }
-  }
-}
+      count: 0,
+    };
+  },
+};
 </script>
