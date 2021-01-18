d8ac992f6f42c6bf0d5b55b88b99a31b8f5e9d64
Stefan Frede <stefan@frede.dev>
2021-01-18 16:37:10 +0100

    refactor: change nav tags (#9)

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,26 @@
         <img alt="Vue.js logo" class="h-8 w-auto" src="./assets/logo.svg" />
         <figcaption>Starter-Kit</figcaption>
       </figure>
-      <div class="space-x-4">
-        <router-link :to="{ name: 'Home' }" class="router-link"
-          >Home</router-link
-        >
-        <router-link :to="{ name: 'About' }" class="router-link"
-          >About</router-link
-        >
-      </div>
+      <ul class="flex space-x-4">
+        <li>
+          <router-link :to="{ name: 'Home' }" class="router-link"
+            >Home</router-link
+          >
+        </li>
+        <li>
+          <router-link :to="{ name: 'About' }" class="router-link"
+            >About</router-link
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/stefanfrede/vuejs-starter-kit/"
+            class="text-base font-bold underline hover:text-green-700"
+          >
+            GitHub
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
   <div class="py-10">
