ad3f8de0ea1d00c846f520b3a6682a96d0265b60
Stefan Frede <stefan@frede.dev>
2021-01-18 16:04:31 +0100

    fix: change classes for active nav status

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import { routes } from './routes.js';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes,
+  linkActiveClass: 'router-link--active',
+  linkExactActiveClass: 'router-link--exact-active',
 });
 
 export default router;
