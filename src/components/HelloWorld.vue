5971508a13f59a40687c332b692f763864c0b008
Stefan Frede <stefan@frede.dev>
2021-01-19 21:09:46 +0100

    feat increase lighthouse score (#11)
    * feat: add meta data
    
    * fix: fix the lighthouse pwa warnings
    
    * fix: add missing robots.txt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,102 +23,87 @@
         <h2 class="mb-8 text-3xl font-bold text-gray-900 leading-tight">
           Installed Plugins
         </h2>
-        <ul class="mb-6 space-x-4 flex">
-          <li>
-            <a
-              href="https://github.com/vuejs/vue-next/tree/master/packages/compiler-sfc#readme"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >@vue/compiler-sfc</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://github.com/vitejs/vite/tree/main/packages/plugin-vue#readme"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >@vitejs/plugin-vue</a
-            >
-          </li>
-        </ul>
+        <p class="mb-6">
+          <a
+            href="https://github.com/vuejs/vue-next/tree/master/packages/compiler-sfc#readme"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >@vue/compiler-sfc</a
+          >
+          -
+          <a
+            href="https://github.com/vitejs/vite/tree/main/packages/plugin-vue#readme"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >@vitejs/plugin-vue</a
+          >
+        </p>
         <h2 class="mt-14 mb-8 text-3xl font-bold text-gray-900 leading-tight">
           Essential Links
         </h2>
-        <ul class="mb-6 space-x-4 flex">
-          <li>
-            <a
-              href="https://v3.vuejs.org/"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >Core Docs</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://forum.vuejs.org"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >Forum</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://chat.vuejs.org"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >Community Chat</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/vuejs"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >Twitter</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://news.vuejs.org"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >News</a
-            >
-          </li>
-        </ul>
+        <p class="mb-6">
+          <a
+            href="https://v3.vuejs.org/"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >Core Docs</a
+          >
+          -
+          <a
+            href="https://forum.vuejs.org"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >Forum</a
+          >
+          -
+          <a
+            href="https://chat.vuejs.org"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >Community Chat</a
+          >
+          -
+          <a
+            href="https://twitter.com/vuejs"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >Twitter</a
+          >
+          -
+          <a
+            href="https://news.vuejs.org"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >News</a
+          >
+        </p>
         <h2 class="mt-14 mb-8 text-3xl font-bold text-gray-900 leading-tight">
           Ecosystem
         </h2>
-        <ul class="space-x-4 flex">
-          <li>
-            <a
-              href="https://router.vuejs.org"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >vue-router</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://vuex.vuejs.org"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >vuex</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://github.com/vuejs/vue-devtools#vue-devtools"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >vue-devtools</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://github.com/vuejs/awesome-vue"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >awesome-vue</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://github.com/vitejs/awesome-vite"
-              class="text-green-600 hover:text-green-700 font-medium underline"
-              >awesome-vite</a
-            >
-          </li>
-        </ul>
+        <p class="mb-6">
+          <a
+            href="https://router.vuejs.org"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >vue-router</a
+          >
+          -
+          <a
+            href="https://vuex.vuejs.org"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >vuex</a
+          >
+          -
+          <a
+            href="https://github.com/vuejs/vue-devtools#vue-devtools"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >vue-devtools</a
+          >
+          -
+          <a
+            href="https://github.com/vuejs/awesome-vue"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >awesome-vue</a
+          >
+          -
+          <a
+            href="https://github.com/vitejs/awesome-vite"
+            class="text-green-600 hover:text-green-700 font-medium underline"
+            >awesome-vite</a
+          >
+        </p>
       </div>
     </div>
   </main>
